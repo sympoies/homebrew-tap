f4b495a0417ae187b830ccf023faca4670ab45f4
graysurf <10785178+graysurf@users.noreply.github.com>
2026-05-21 09:05:22 +0800

    chore(formula): bump nils-cli to v0.13.0
    - Update macOS/Linux URLs and sha256 to v0.13.0 release artifacts.

--- Formula/nils-cli.rb
+++ Formula/nils-cli.rb
@@ -5,21 +5,21 @@ class NilsCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.12.0/nils-cli-v0.12.0-aarch64-apple-darwin.tar.gz"
-      sha256 "97e14656d07b0d5833ff8e1e1f17e8df6191f2b8636a5e84f2a1de65462b819f"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.13.0/nils-cli-v0.13.0-aarch64-apple-darwin.tar.gz"
+      sha256 "1863cbd0c9d780c7fefdb10527ba0e7a549a371c77f19bc805e389db9bbd8b1c"
     else
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.12.0/nils-cli-v0.12.0-x86_64-apple-darwin.tar.gz"
-      sha256 "db32894869be42bdf732122e51a15aaf5f2c2f7b491b158ab95114a553162a3a"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.13.0/nils-cli-v0.13.0-x86_64-apple-darwin.tar.gz"
+      sha256 "a73ff9f8686e8b6fea85c80ce3291620e4b6164428d021c27d940a45b005bdc1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.12.0/nils-cli-v0.12.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8ce4861d3e88af14bfac5f37841ca3711e0ce390762aac06d5f5fbacd535d45f"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.13.0/nils-cli-v0.13.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "dcd862d0442b60409b6f168c2b455f8150a5d0032d25e81658ac5a7bd7cf210e"
     else
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.12.0/nils-cli-v0.12.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0b614071db4ffbc3e21afa8aad9f7899ba17a8250e01302b3d8ac0eb6b13455c"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.13.0/nils-cli-v0.13.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a6c43c16821b41e192a8b27f665793de1b12648085cf9e854c4783bcc64d630d"
     end
   end
 
